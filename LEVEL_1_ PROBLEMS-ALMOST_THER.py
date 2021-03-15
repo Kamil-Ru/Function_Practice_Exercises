7c90202d1097e5b6f49e1c179fa822f3c9219c20
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-03-15 20:55:32 +0100

    Completed, but correctly?

--- LEVEL_1_ PROBLEMS-ALMOST_THER.py
+++ LEVEL_1_ PROBLEMS-ALMOST_THER.py
@@ -5,3 +5,17 @@
 # almost_there(209) --> True
 # NOTE: abs(num) returns the absolute value of a number
 
+def almost_there(n):
+    if n <= 110 and n >= 90:
+        return True
+    elif n <= 210 and n >= 190:
+        return True
+    else:
+        return False
+
+# Test
+
+print(almost_there(104))    # --> True?
+print(almost_there(150))    # --> False?
+print(almost_there(90))     # --> True?
+print(almost_there(209))    # --> True?
