7ff41ce48926bca3f855bbdc20816b0f1eae9daf
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-03-15 20:07:46 +0100

    exercise completed

---
+++ MAKES_TWENTY.py
@@ -0,0 +1,22 @@
+# MAKES TWENTY: Given two integers, 
+# return True if the sum of the integers is 20 or if one of the integers is 20. 
+# If not, return False
+
+# makes_twenty(20,10) --> True
+# makes_twenty(12,8) --> True
+# makes_twenty(2,3) --> False
+
+
+def makes_twenty(x,y):
+    if x + y == 20:
+        return True
+    elif x == 20 or y == 20:
+        return True
+    else:
+        return False
+
+# Test
+print(makes_twenty(20,10))
+print(makes_twenty(12,8))
+print(makes_twenty(2,3))
+print(makes_twenty(10,30))
