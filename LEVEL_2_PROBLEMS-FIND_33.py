861f87036c014beba6a4b0658288b37330139690
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-03-15 21:18:28 +0100

    I'm stuck

--- LEVEL_2_PROBLEMS-FIND_33.py
+++ LEVEL_2_PROBLEMS-FIND_33.py
@@ -2,3 +2,17 @@
 # Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
 
 # has_33([1, 3, 3]) → True has_33([1, 3, 1, 3]) → False has_33([3, 1, 3]) → False
+
+
+def has_33(nums):
+    for x in range(len(nums)):
+        if nums[x] == 3 and nums[x+1] == 3 or nums[x] == 3 and nums[x-1]:
+            return True
+            
+        else:
+            continue
+    return False
+    
+    
+ # WRONG !!!!!!!
+    
