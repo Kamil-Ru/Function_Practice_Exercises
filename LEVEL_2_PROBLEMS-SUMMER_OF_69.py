ca09d68e11b341df478d678547d24d49cc6dc787
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-03-16 20:48:18 +0100

    Exercise completed

--- LEVEL_2_PROBLEMS-SUMMER_OF_69.py
+++ LEVEL_2_PROBLEMS-SUMMER_OF_69.py
@@ -3,4 +3,29 @@
 # (every 6 will be followed by at least one 9). Return 0 for no numbers.
 
 
-# summer_69([1, 3, 5]) --> 9 summer_69([4, 5, 6, 7, 8, 9]) --> 9 summer_69([2, 1, 6, 9, 11]) --> 14
+# summer_69([1, 3, 5]) --> 9 
+# summer_69([4, 5, 6, 7, 8, 9]) --> 9 
+# summer_69([2, 1, 6, 9, 11]) --> 14
+
+
+def summer_69(_list):
+    a = 0
+    stop = 0
+    for x in _list:
+        if x < 6 and stop == 0:
+            a = a + x
+        elif x == 6:
+            stop = 1
+        elif x == 9:
+            stop = 0
+        elif x > 9 and stop == 0:
+            a = a + x      
+        else:
+            continue
+    return a
+
+print(summer_69([1, 3, 5]))
+print(summer_69([4, 5, 6, 7, 8, 9]))
+print(summer_69([2, 1, 6, 9, 11]))
+print(summer_69([1, 4, 6, 1, 1, 1, 1, 9, 10]))
+print(summer_69([1, 4, 6, 1, 1, 1, 1, 9, 10, 6, 1000, 10000, 9]))
